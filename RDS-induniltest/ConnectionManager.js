0cfdb05d51b92a6f56d3919fa0b4ea4cbddfead4
Indunil1992 <indunil@adroitlogic.com>
2019-07-23 11:57:46 +0530

    j

--- RDS-induniltest/ConnectionManager.js
+++ RDS-induniltest/ConnectionManager.js
@@ -1,11 +1,11 @@
 module.exports = function() {
     this.dbConnections = [];
 
-    this.dbConnections["instance1"] = {
-        host: process.env.EndPoint_rdsInstance1,
-        port: process.env.Port_rdsInstance1,
-        user: process.env.User_rdsInstance1,
-        password: process.env.Password_rdsInstance1,
-        database: "induniltestdb"
+    this.dbConnections["INDinstance2"] = {
+        host: process.env.EndPoint_rdsINDinstance2,
+        port: process.env.Port_rdsINDinstance2,
+        user: process.env.User_rdsINDinstance2,
+        password: process.env.Password_rdsINDinstance2,
+        database: "indpalitha"
     };
 };
--- RDS-induniltest/lambda.js
+++ RDS-induniltest/lambda.js
@@ -4,27 +4,14 @@ const rds = new SL_AWS.RDS(connectionManager);
 
 exports.handler = function (event, context, callback) {
 
-
-    // You can pass the existing connection to this function.
-    // A new connection will be created if it's not present as the third param 
     // You must always end/destroy the DB connection after it's used
-    rds.query({
-        instanceIdentifier: 'instance1',
-        query: 'CREATE TABLE Persons1(     PersonID int,     LastName varchar(255),     FirstName varchar(255),     Address varchar(255),     City varchar(255)  );',
-        inserts: [2, "hera", "sach", "no142 pallekele", "Kandy"]
-    }, function (error, results, connection) {
+    rds.beginTransaction({
+        instanceIdentifier: 'INDinstance2'
+    }, function (error, connection) {
         if (error) {
-            console.log("Error occurred");
             throw error;
-        } else {
-            console.log("Success")
-            console.log(results);
         }
-
-        connection.end();
     });
-
-
 
 
     callback(null, { "message": "Successfully executed" });
